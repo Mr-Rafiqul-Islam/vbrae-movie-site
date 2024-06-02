f0db042c58fc8b622c871668d3c801e90ed35c5e
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-06-02 20:54:26 +0600

    header done

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
+import { Link } from 'react-router-dom'
+import heart from '../assets/icons/heart (1).svg'
+import dollar from '../assets/icons/Subtração 165.svg'
+import bag from '../assets/icons/Subtraction 124.svg'
+import userIcon from '../assets/icons/path0.svg'
 
 function Header() {
   return (
-    <div>
-      <button className='py-2 px-4 bg-gradient-to-r from-gradF to-gradT'>
-        <a href='/'>Home</a>
-      </button>
-    </div>
+    <>
+    <section className='pt-[50px] pb-[37px]'>
+      <div className="container">
+     <div className="flex justify-between">
+      <Link to={'/'}><img src={logo} alt="logo" /></Link>
+      <div className='relative '>
+        <input type="text" placeholder='Search products, sellers, deals...' className='w-[650px] border border-gray-300 px-4 py-2 rounded-lg bg-[#1E2127] text-white placeholder:text-white'/>
+      </div>
+
+      <div className='flex justify-end gap-5'>
+        <img src={dollar} alt="" className='cursor-pointer '/>
+        <img src={heart} alt="" className='cursor-pointer '/>
+        <img src={bag} alt="" className='cursor-pointer '/>
+        <img src={userIcon} alt="" className='cursor-pointer '/>
+      </div>
+     </div>
+      </div>
+    </section>
+    <hr className='container'/>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='text-3xl'>i am a website
+    <div className='text-3xl'>
     </div>
   )
 }

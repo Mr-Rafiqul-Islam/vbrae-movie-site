36f49c18c99842dff1c2568db9f3ccd4d81d2cc7
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-06-07 02:45:32 +0600

    review card create

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import user1 from "../assets/"
+function ReviewCard() {
+  return (
+    <div className="pt-[43px] pb-[41px] ps-[38px]">
+        <div className="flex gap-4">
+            <Image src={} className={``}/>
+        </div>
+    </div>
+  )
+}
+
+export default ReviewCard

ee6bfd55a304425b93771bc39eccc6c8c7e70855
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2024-06-07 23:03:58 +0600

    slider updated

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import user1 from "../assets/images/rev1.png"
-import stargrp from "../assets/icons/stargrp.png"
-import Image from './Image'
-function ReviewCard() {
+import React from "react";
+import Image from "./Image";
+function ReviewCard({name, review, rating, img}) {
   return (
-    <div className="!pt-[13px] md:!pt-[43px] !pb-[15px] md:!pb-[41px] !ps-[15px] md:!ps-[38px] border-gradient">
-        <div className="flex gap-4 mb-[7px] md:mb-[27px]">
-            <Image src={user1} className={`inline-block w-7 h-7 md:w-14 md:h-14`}/>
-            <div className="flex flex-col gap-1 md:gap-4">
-                <h4 className='font-secondary font-medium text-[12px] md:text-2xl text-white'>Username</h4>
-                <Image src={stargrp} className={`inline-block w-[73.29px] h-[11.95px] md:w-[164.15px] md:h-[26.77px]`} />
-            </div>
+    <div className="!pt-[13px] md:!pt-[43px] md:!pb-[41px] md:!ps-[38px] !pb-[15px] !ps-[15px]  border-gradient">
+      <div className="flex gap-4 mb-[7px] md:mb-[27px]">
+        <Image src={img} className={`inline-block w-7 h-7 md:w-14 md:h-14`} />
+        <div className="flex flex-col gap-1 md:gap-4">
+          <h4 className="font-secondary font-medium text-[12px] md:text-2xl text-white">
+            {name}
+          </h4>
+          <Image
+            src={rating}
+            className={`inline-block w-[73.29px] h-[11.95px] md:w-[164.15px] md:h-[26.77px]`}
+          />
         </div>
-        <p className='font-secondary font-normal text-[12px] md:text-[22px] text-white w-[172px] h-auto md:w-[426px] md:h-[64px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      </div>
+      
+        <div>
+        <p className="font-secondary font-normal text-[12px]  md:text-[16px] text-white ">
+          {review}
+        </p>
+        </div>
+      
     </div>
-  )
+  );
 }
 
-export default ReviewCard
+export default ReviewCard;
--- src/layouts/Review.jsx
+++ src/layouts/Review.jsx
@@ -3,13 +3,62 @@ import Image from "../components/Image";
 import Line from "../assets/icons/revLine.png";
 import ReviewStar from "../assets/icons/ReviewStar.svg";
 import ReviewCard from "../components/ReviewCard";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import user1 from "../assets/images/rev1.png";
+import user2 from "../assets/images/rev2.png";
+import user3 from "../assets/images/rev3.png";
+import stargrp from "../assets/icons/stargrp.png";
 function Review() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <section className="py-[72px]">
       <div className="container">
         <div className="flex">
-          <div className="grid place-content-center w-1/4">
+          <div className="grid place-content-center max-w-[210px]">
             <div className="text-center">
               <h3 className="font-third font-bold text-[12px] md:text-[36px] text-white">
                 Trustpilot <br /> Reviews
@@ -26,8 +75,34 @@ function Review() {
               </div>
             </div>
           </div>
-          <div className="w-3/4">
-            <ReviewCard />
+          <div className="w-[87%] review">
+            <Slider {...settings}>
+              <ReviewCard
+                img={user1}
+                name={"Username"}
+                rating={stargrp}
+                review={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsumdolor amet sit."
+                }
+              />
+              <ReviewCard
+                img={user2}
+                name={"Username"}
+                rating={stargrp}
+                review={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsumdolor amet sit."
+                }
+              />
+              <ReviewCard
+                img={user3}
+                name={"Username"}
+                rating={stargrp}
+                review={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem ipsumdolor amet sit."
+                }
+              />
+              
+            </Slider>
           </div>
         </div>
       </div>
